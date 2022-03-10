0e82eb15e1d19727239f885aabe53d4ff7afa5f2
Anuj Yadav <anujyadav851@gmail.com>
2022-03-10 15:55:38 +0530

    REST API Caller/Consumer code added

---
+++ src/test/java/sprinbootConsumer/RestBooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sprinbootConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
